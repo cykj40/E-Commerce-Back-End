bb78137f0f5f492a5b6574d4447bf344067b37a9
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-09-18 12:30:41 -0400

    Update server.js
    i missed the import sequelize connection added it.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sequelize = require('sequelize');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,5 +18,6 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
 });
+
 
 
