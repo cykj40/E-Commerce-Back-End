a9308c28cfa59ece28f406ebe642a7b03033c977
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-09-19 10:00:26 -0400

    Update server.js
    declared sequelize twice had to get rid of the incorrect syntax. im not sure if i did it or it was laready in the starter code but regardless one of them had to go because one of them had the wrong syntax and you cant declare the same variable twice

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const sequelize = require('sequelize');
 const routes = require('./routes');
-// import sequelize connection
+//  sequelize connection
 const sequelize = require('./config/connection');
 
 const app = express();
